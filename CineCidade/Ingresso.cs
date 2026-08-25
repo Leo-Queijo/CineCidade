47b67537e22836fd624c298478381993b1c58ce6
Leonardo <leoqueijo8@hotmail.com>
2026-08-24 22:16:08 -0300

    feat: Solicitações ao usuario

--- CineCidade/Ingresso.cs
+++ CineCidade/Ingresso.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CineCidade
+﻿namespace CineCidade
 {
-    internal class Ingresso
+    class Ingresso
     {
+        public string nomeCliente;
+        public int qtdIngresso;
+        public string cupom;
+        public int idadeCliente;
+
+        public double CalcularValorBruto()
+        {
+            return 5.00 + (qtdIngresso * 25.00);
+        }
+
+        public double CalcularDesconto()
+        {
+            if (cupom.ToUpper() == "CINEMA10")
+            {
+                return CalcularValorBruto() * 0.10;
+            }
+
+            return 0.00;
+        }
+
+        public double CalcularValorFinal()
+        {
+            return CalcularValorBruto() - CalcularDesconto();
+        }
+
+        public bool ElegivelSessaoNoturna()
+        {
+            return qtdIngresso > 1 && idadeCliente >= 18;
+        }
     }
 }
--- CineCidade/Program.cs
+++ CineCidade/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CineCidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCliente;
+            int qtdIngresso;
+            string cupom;
+            int idadeCliente;
+
+            Console.Write("Digite o nome do cliente: ");
+            nomeCliente = Console.ReadLine() ?? "";
+
+            Console.Write("Digite a quantidade de ingressos: ");
+            qtdIngresso = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.Write("Digite o cupom de desconto: ");
+            cupom = Console.ReadLine() ?? "";
+
+            Console.Write("Digite a idade do cliente: ");
+            idadeCliente = int.Parse(Console.ReadLine() ?? "0");
+
+            Ingresso ingresso = new Ingresso();
+
+            ingresso.nomeCliente = nomeCliente;
+            ingresso.qtdIngresso = qtdIngresso;
+            ingresso.cupom = cupom;
+            ingresso.idadeCliente = idadeCliente;
+
+            Console.WriteLine();
+            Console.WriteLine("===== RESUMO DA COMPRA =====");
+
+            Console.WriteLine($"Cliente: {ingresso.nomeCliente.ToUpper()}");
+            Console.WriteLine($"Valor bruto: R$ {ingresso.CalcularValorBruto():F2}");
+            Console.WriteLine($"Valor final: R$ {ingresso.CalcularValorFinal():F2}");
+            Console.WriteLine($"Elegível para Sessão Noturna: {ingresso.ElegivelSessaoNoturna()}");
+        }
+    }
+}
